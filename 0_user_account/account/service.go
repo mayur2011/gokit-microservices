980b6d46d427344e8f396d62da4922a9a105f1dd
mayur2011 <er.mayur2011@gmail.com>
2023-06-22 21:46:22 +0530

    gokit user account msvc implmentation

--- 0_user_account/account/service.go
+++ 0_user_account/account/service.go
@@ -3,7 +3,7 @@ package account
 import "context"
 
 //Service which will be exposed to outside users
-type Service interfacee{
+type Service interface {
 	CreateUser(ctx context.Context, email string, password string) (string, error)
 	GetUser(ctx context.Context, id string) (string, error)
 }
--- 0_user_account/account/user.go
+++ 0_user_account/account/user.go
@@ -1,6 +1,6 @@
 package account
 
-type User struct{
+type User struct {
 	ID string `json:"id"`
 	Email string `json:"email"`
 	Password string `json:"password"`
