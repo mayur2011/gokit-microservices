523ca2ec4c5a41ffb5b23b958d53cf41e519744f
mayur2011 <er.mayur2011@gmail.com>
2023-06-23 19:31:32 +0530

    account msvc implementation using gokit completed

--- 0_user_account/account/endpoint.go
+++ 0_user_account/account/endpoint.go
@@ -20,11 +20,15 @@ func MakeEndpoints(s Service) Endpoints {
 
 func makeCreateUserEndpoint(s Service) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-
+		req := request.(CreateUserRequest)
+		ok, err := s.CreateUser(ctx, req.Email, req.Password)
+		return CreateUserResponse{Ok: ok}, err
 	}
 }
 func makeGetUserEndpoint(s Service) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (interface{}, error) {
-
+		req := request.(GetUserRequest)
+		email, err := s.GetUser(ctx, req.Id)
+		return GetUserResponse{Email: email}, err
 	}
 }
